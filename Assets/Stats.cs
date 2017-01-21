1d9b711117979245312a0a18e035be59d7644d3d
Oskari Mäki <oskari.maki@hotmail.com>
2017-01-21 02:38:46 +0200

    blood stuff
    :100:

--- Assets/Stats.cs
+++ Assets/Stats.cs
@@ -7,13 +7,26 @@ public class Stats : MonoBehaviour
     public int Health = 0;
     public bool isAlive = true;
 
+    public GameObject blood;
+
+    public int bloodParticleAmount = 100;
+
     public void TakeDamage(int damage)
     {
+        Debug.Log("VITTU OSU");
         Health -= damage;
         if (Health <= 0 && isAlive)
         {
             isAlive = false;
             Die();
+        }
+
+        for(int i = 0; i < bloodParticleAmount; i++)
+        {
+            Debug.Log("asd");
+            GameObject a = Instantiate(blood, new Vector3(transform.position.x, transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+            Rigidbody aR = a.GetComponent<Rigidbody>();
+            aR.AddForce(Vector3.forward * 100);
         }
             
     }
