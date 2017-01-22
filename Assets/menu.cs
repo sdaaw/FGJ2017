17bfc1fff5310d6b3d28e3a109b62b3fa8b9ee8b
Ati Marin <ati.marin1@gmail.com>
2017-01-22 09:55:18 +0200

    asd

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class menu : MonoBehaviour
 {
+    public Animator m_anim;
     public GameObject start;
     public GameObject controls;
+
+    private bool hasPlayedStartAnim = false;
 
     private void Awake()
     {
         start.SetActive(true);
         controls.SetActive(false);
+        hasPlayedStartAnim = true;
     }
 
     public void StartGame()
@@ -21,6 +25,8 @@ public class menu : MonoBehaviour
 
     public void DisplayStartScreen()
     {
+        if (hasPlayedStartAnim)
+            m_anim.speed = 100;
         start.SetActive(true);
         controls.SetActive(false);
     }
