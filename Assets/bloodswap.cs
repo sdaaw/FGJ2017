5f441a7e7087ebd58e7e669459c5868d655d57a0
Ati Marin <ati.marin1@gmail.com>
2017-01-22 10:27:06 +0200

    bloodstufflel

---
+++ Assets/bloodswap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloodswap : MonoBehaviour
+{
+    public Texture s1;
+    public Texture s2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Renderer>().material.mainTexture = (Random.Range(0, 2) == 1) ? s1 : s2;
+	}
+}
